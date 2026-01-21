14bb6d23f90852555c8a53e96f0cfa13d88079c8
Lance Thongsavanh <lancer8106@gmail.com>
2026-01-21 16:21:13 -0500

    Added details to header. Prompts the user for a valid input for the bill

--- Lab2_LanceTho-#.py
+++ Lab2_LanceTho-#.py
@@ -1,5 +1,25 @@
-#The program's name,
-#Your name (the author),
-#The purpose of the program,
-#Any info about starter code (If used, where it came from, link, etc.), and the
-#Date.
+#Restaurant Tip Calculator
+#Lance Thongsavanh
+#As a restaurant owner, you need a program that calculates tip suggestions for your customers.
+#1/21/2026
+
+#Requirements:
+#Prompt the user for the total of a dinner bill
+#Calculate a 15% tip and a 20% tip
+#Print the suggested tip amounts
+#Print the total bill amount including the 15$ tip, and then again with the 20% tip
+#You must use f-strings for all output
+#Currency should be correctly displayed
+
+bill: float = 0
+width: int = 20
+prompt: str = "Please enter the total of a dinner bill"
+error: str ="Invalid total"
+print(f"|{prompt:{width}}|")
+bill = float(input())
+while(bill < 0 or bill == 0):
+    print(f"|{error:{width}}|")
+    print(f"|{prompt:{width}}|")
+    bill = float(input())
+
+
