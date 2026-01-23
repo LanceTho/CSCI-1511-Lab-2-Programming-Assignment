21ca22345a1487cedf62de91e3912b1c922497bf
Lance Thongsavanh <lancer8106@gmail.com>
2026-01-23 13:49:52 -0500

    Added documentation for the program and what it does. Finished, ready to grade.

--- Lab2_LanceTho-#.py
+++ Lab2_LanceTho-#.py
@@ -1,16 +1,9 @@
 #Restaurant Tip Calculator
 #Lance Thongsavanh
 #As a restaurant owner, you need a program that calculates tip suggestions for your customers.
-#1/21/2026
+#1/23/2026
 
-#Requirements:
-#Prompt the user for the total of a dinner bill
-#Calculate a 15% tip and a 20% tip
-#Print the suggested tip amounts
-#Print the total bill amount including the 15$ tip, and then again with the 20% tip
-#You must use f-strings for all output
-#Currency should be correctly displayed
-
+#Declares variables
 bill: float = 0
 prompt: str = "Please enter the total of a dinner bill (exclude the '$')"
 error: str = "Invalid total"
@@ -20,6 +13,7 @@ recipt_20: str = "20%:"
 tip_15: float = 0
 tip_20: float = 0
 
+#Prompts the user for the bill and loops if the user provides an invalid value
 print(f"{prompt:<{20}}")
 bill = float(input())
 while(bill <= 0):
@@ -27,13 +21,17 @@ while(bill <= 0):
     print(f"{prompt:{20}}")
     bill = float(input())
 
+#Calculates tip for 15% and 20%
 tip_15 = bill*0.15
 tip_20 = bill*0.2
 
+#Outputs the total
 print(f"|{recipt_total:<{10}}" + f"${bill:.2f}|")
 
+#Outputs the 15% tip and total with it added
 print(f"|{recipt_15:<{11}}" + f"${tip_15:.2f}|")
 print(f"|{recipt_total:{10}}" + f"${(bill + tip_15):.2f}|")
 
+#Outputs the 20% tip and total with it added
 print(f"|{recipt_20:<{10}}" + f"${tip_20:.2f}|")
 print(f"|{recipt_total:{10}}" + f"${(bill + tip_20):.2f}|")
